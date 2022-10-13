72735c67e8aaa916eb09d71dd6a2b44a032a4496
arpifytech <107615012+arpifytech@users.noreply.github.com>
2022-10-13 14:58:06 +0400

    create download urel for blob

--- src/azure-file-upload.service.ts
+++ src/azure-file-upload.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { BlobServiceClient } from '@azure/storage-blob';
+import {
+  BlobServiceClient,
+  generateBlobSASQueryParameters,
+  ContainerSASPermissions,
+  BlockBlobClient,
+} from '@azure/storage-blob';
 import { v1 as uuidv1 } from 'uuid';
 import { ConfigService } from '@nestjs/config';
 
@@ -21,14 +26,13 @@ export class AzureFileUploadService {
     );
   }
 
-  async uploadBlobFile(file: any, containerName: string) {
+  async uploadBlobFile(file: any, containerName: string): Promise<any> {
     // Create a unique name for the blob
     const blobName = `${file.originalname
       .split('.')
       .shift()}-${uuidv1()}.${file.originalname.split('.').pop()}`;
-    const containerClient = this.blobServiceClient.getContainerClient(
-      containerName,
-    );
+    const containerClient =
+      this.blobServiceClient.getContainerClient(containerName);
     // Get a block blob client
     const blockBlobClient = containerClient.getBlockBlobClient(blobName);
 
@@ -46,10 +50,9 @@ export class AzureFileUploadService {
     return blobName;
   }
 
-  async getBlobObject(containerName: string, blobName: string) {
-    const containerClient = this.blobServiceClient.getContainerClient(
-      containerName,
-    );
+  async getBlobObject(containerName: string, blobName: string): Promise<any> {
+    const containerClient =
+      this.blobServiceClient.getContainerClient(containerName);
     // Get a block blob client
     const blockBlobClient = containerClient.getBlockBlobClient(blobName);
     const blobBuffer = await blockBlobClient.downloadToBuffer();
@@ -60,22 +63,40 @@ export class AzureFileUploadService {
     };
   }
 
-  async deleteBlobObject(containerName: string, blobName: string) {
-    const containerClient = this.blobServiceClient.getContainerClient(
-      containerName,
+  async getDownloadUrl(blob: { containerName: string; blobName: string }): Promise<any> {
+    const {containerName, blobName} = blob;
+    const blobServiceClient = new BlobServiceClient(
+      this.blobServiceClient.url,
+      this.blobServiceClient.credential,
     );
+    const blobSAS = generateBlobSASQueryParameters(
+      {
+        containerName,
+        blobName,
+        permissions: ContainerSASPermissions.parse('r'),
+        startsOn: new Date(),
+        expiresOn: new Date(new Date().valueOf() + 5 * 60 * 1000),
+      },
+      this.blobServiceClient.credential,
+    ).toString();
+    const sasUrl = `${blobServiceClient.url}${containerName}/${blobName}?${blobSAS}`;
+    return sasUrl;
+  }
+
+  async deleteBlobObject(containerName: string, blobName: string) {
+    const containerClient =
+      this.blobServiceClient.getContainerClient(containerName);
     // Get a block blob client
     const blockBlobClient = containerClient.getBlockBlobClient(blobName);
     return await blockBlobClient.deleteIfExists();
   }
 
-  async createContainer(containerName: string) {
+  async createContainer(containerName: string): Promise<any> {
     // Create a unique name for the container
     containerName = containerName + uuidv1();
     // Get a reference to a container
-    const containerClient = this.blobServiceClient.getContainerClient(
-      containerName,
-    );
+    const containerClient =
+      this.blobServiceClient.getContainerClient(containerName);
 
     // Create the container
     const createContainerResponse = await containerClient.create();
@@ -88,7 +109,7 @@ export class AzureFileUploadService {
   }
 
   async getContainers() {
-    let containers = [];
+    const containers = [];
     let i = 1;
     for await (const container of this.blobServiceClient.listContainers()) {
       console.log(`Container ${i++}: ${container.name}`);
@@ -98,9 +119,8 @@ export class AzureFileUploadService {
   }
 
   async getContainersContent(containerName: string) {
-    const containerClient = this.blobServiceClient.getContainerClient(
-      containerName,
-    );
+    const containerClient =
+      this.blobServiceClient.getContainerClient(containerName);
     const content = [];
     for await (const item of containerClient.listBlobsByHierarchy('/')) {
       if (item.kind === 'prefix') {
@@ -123,9 +143,8 @@ export class AzureFileUploadService {
     const { contentBase, fileName, containerName } = baseInputDto;
     // Create a unique name for the blob
     const blobName = `${fileName.split('.').shift()}-${uuidv1()}.base64`;
-    const containerClient = this.blobServiceClient.getContainerClient(
-      containerName,
-    );
+    const containerClient =
+      this.blobServiceClient.getContainerClient(containerName);
     // Get a block blob client
     const blockBlobClient = containerClient.getBlockBlobClient(blobName);
 
@@ -141,5 +160,15 @@ export class AzureFileUploadService {
       uploadBlobResponse.requestId,
     );
     return blobName;
+  }
+
+  async accessResource(request: {url: string}): Promise<any> {
+    try {
+      const blockBlobClient = new BlockBlobClient(request.url);
+      await blockBlobClient.downloadToFile(__dirname + '/../nkar.jpeg');
+  } catch (err) {
+    console.log(err)
+  }
+    return __dirname;
   }
 }
